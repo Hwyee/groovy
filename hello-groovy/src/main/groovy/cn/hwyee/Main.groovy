fa43942184c04ec33b5ceb8417240c9430d66edf
hwyee <hwyee@foxmail.com>
2023-04-30 20:21:09 +0800

    hello-groovy

---
+++ hello-groovy/src/main/groovy/cn/hwyee/Main.groovy
@@ -0,0 +1,13 @@
+package cn.hwyee
+
+/**
+ * @author hwyee@foxmail.com
+ * @version 1.0
+ * @ClassName ${NAME}
+ * @description
+ * @date 2023/4/30
+ * @since JDK 1.8
+ */
+static void main(String[] args) {
+    println "Hello world!"
+}
